26179f5170f6545d8d1397f0fcdc5e168c86a04d
Arbieboy <arbieboy021@gmail.com>
2024-10-03 09:51:39 +0800

    Add files via upload

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+
+	cout << "Our Lady of Lourdes College Foundation" << endl;
+	cout << "College of Computer Studies" << endl;
+	cout << "BSIT-1A" << endl;
+	cout << "Arbie M. Dasco" << endl;
+	cout << "Brgy. Talobatib Labo Cam. Norte" << endl;
+
+	return 0;
+}
