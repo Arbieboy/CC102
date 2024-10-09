f9ef9d865e3abca3d8afd4895989493e079ba8ab
Arbieboy <arbieboy021@gmail.com>
2024-10-09 15:34:49 +0800

    05DataTypesOperations.cpp

--- 05DataTypesOperations.cpp
+++ 05DataTypesOperations.cpp
@@ -16,7 +16,7 @@ int main() {
     cout << "Sum: " << num1 + num2 << endl;
     cout << "Difference: " << num1 - num2 << endl;
     cout << "Product: " << num1 * num2 << endl;
-    cout << "Quotient: " << (num2 != 0 ? num1 / num2 : 0) << endl; 
+    cout << "Quotient: " << num1 / num2 << endl; 
     cout << "Remainder: " << (static_cast<int>(num2) != 0 ? static_cast<int>(num1) % static_cast<int>(num2) : 0) << endl;
 
     return 0;
