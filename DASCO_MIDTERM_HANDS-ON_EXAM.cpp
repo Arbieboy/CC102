9e447675d4b4fbcd3d48ca361bf0ff905be954e5
Arbieboy <arbieboy021@gmail.com>
2024-10-15 10:07:15 +0800

    Add files via upload

---
+++ DASCO_MIDTERM_HANDS-ON_EXAM.cpp
@@ -0,0 +1,44 @@
+#include <iostream> 
+
+using namespace std;
+
+int main() {
+	double sugarPriceUSD, ricePricePound, sardinesPricePound, coffeePriceUSD, milkPriceUSD;	
+	double sugarQty, riceQty, sardinesQty, coffeeQty, milkQty;
+	double usdToPhp, poundToPhp, convertionRate
+	double totalSugarcost, totalSardinescost, totalRicecost, totalCoffeecost, totalMilkcost;
+	double USDToPHP = 59.0;
+	double PoundToPHP = 72.0;
+	
+	cout << "ENTER THE PRICE OF THE SUGAR (USD):\n";
+	cin >>  sugarPriceUSD;
+	cout << "ENTER THE PRICE OF RICE (Pound)";
+	cin >> ricePricePound;
+	cout << "ENTER THE PRICE OF SARDINES (Pound)";
+	cin >> sardinesPricePound;
+	cout << "ENTER THE PRICE OF COFFEE (USD)";
+	cin >> coffeePriceUSD;
+	
+	cout << "Enter the quantity of sugar	:\n";
+	cin >> sugarQty;
+	cout << "Enter the qantity of rice	:\n";
+	cin >> riceQty;
+	cout << "Enter the quantity of sardines	:\n";
+	cin >> sardinesQty;
+	cout << "Enter the quantity of coffee	:\n";
+	cin >> coffeeQty;
+	cout << "Enter the quantity of milk	:\n";
+	cin >> milkQty;
+	
+	double sugarPricePhp = sugarPriceUSD * USDtoPHP
+	double ricePricePhp = ricePricePound * USStoPHP
+	double sardinesPricePhp = sardinesPricePuond * USDtoPHP
+	double coffeePricePhp = coffeePriceUSD * USDtoPHP
+	double milkPricePhp = milkPriceUSD * USDtoPHP
+	
+	double totalSugarcost = 
+	
+	
+	
+	return 0;
+}
