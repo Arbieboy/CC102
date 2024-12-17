c8ca3699c560cb19f076b4a6365371dfdca0a709
Arbieboy <arbieboy021@gmail.com>
2024-12-17 15:44:20 +0800

    Add files via upload

---
+++ Pattern2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int rows = 8;
+	
+	for (int a = 1; a <= rows; a++) {
+		for (int b = 1; b <= rows - a; b++) {
+			cout << "  "; 
+		}
+		for (int b = a; b>=1; b--) {
+			if (b % 3 == 0 ) {
+				cout << "* ";
+			} 
+			else if (b % 5 == 0 ) {
+				cout << "# ";
+			}
+			else if (b % 7 == 0) {
+				cout << "@ ";
+			}else{
+				cout << b << " ";	
+			}
+		}
+		
+		cout << endl;
+	}
+	
+	return 0;
+}
